c22b2801e79e3c707225343fd32ef8be3f518bc6
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2025-08-12 23:32:43 -0300

    remove firebaseConnection do rastreamwnto e adiciona no gitignore

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './home.css';
 // Página Home
 // - Lista pública de todos os posts, mostrando título, autor e data.
 // - Cada título é clicável e leva para a página de detalhes do post.
 
-const Home = ({ posts }) => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
+
+import { db } from '../../firebaseConnection';
+import { collection, onSnapshot } from 'firebase/firestore';
+
+const Home = () => {
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    const unsub = onSnapshot(collection(db, 'posts'), (snapshot) => {
+      let listaPost = [];
+
+      snapshot.forEach((doc) => {
+        listaPost.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          resumo: doc.data().resumo,
+        });
+      });
+      setPosts(listaPost);
+    });
+    return () => unsub();
+  }, []);
+
   return (
     <div className="conteiner-post">
       {posts.map((post) => {
         return (
-          <div key={post.id} className="post-home">
-            <Link to={`/post/${post.id}`}>
+          <Link to={`/post/${post.id}`} key={post.id}>
+            <div className="post-home">
               <h2 className="post-titulo">{post.titulo}</h2>
-              <p className="post-conteudo">{post.conteudo}</p>
-            </Link>
-          </div>
+              <p className="post-conteudo">{post.resumo}</p>
+            </div>
+          </Link>
         );
       })}
     </div>
